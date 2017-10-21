b64bdf5fc5272ce63a05c3d0d6ff9438051e7903
Nathan Gane <nathanspencer10@hotmail.com>
2017-10-21 15:51:45 +0800

    Adding nathans stuff

---
+++ Assets/Sports-Kinematics/Scripts/CreativeSolutions/PlayerStriker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SportsKinematics
+{
+    public class PlayerStriker : Striker
+    {
+        SortedList<float, StrikerData> m_playerStrikerData = new SortedList<float, StrikerData>();
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (GetTime() > 0 && !m_playerStrikerData.ContainsKey(GetTime()))
+            {
+                StrikerData tmp = new StrikerData();
+                tmp.position = this.transform.position;
+                tmp.rotation = this.transform.rotation.eulerAngles;
+
+                m_playerStrikerData.Add(GetTime(), tmp);
+            }
+        }
+
+        public void ClearData()
+        {
+            m_playerStrikerData = new SortedList<float, StrikerData>();
+        }
+
+        public SortedList<float, StrikerData> GetData()
+        {
+            return m_playerStrikerData;
+        }
+
+        float GetTime()
+        {
+            return 0;//m_simManager.GetComponent<SimulationManager>().m_time;
+        }
+
+        public void SetOccluded(bool newOcclusion)
+        {
+            if(m_currentStriker != null)
+                m_currentStriker.GetComponent<Renderer>().enabled = newOcclusion;
+        }
+    }
+}
---
+++ Assets/Sports-Kinematics/Scripts/CreativeSolutions/Striker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SportsKinematics
+{
+    public class Striker : MonoBehaviour
+    {
+
+        public GameObject m_tableTennisBat;
+        public GameObject m_catch;
+        public GameObject m_simManager;
+
+        protected GameObject m_currentStriker;
+
+        public enum StrikerSelection
+        {
+            //catch renderes no striker object
+            Catch = 0,
+            //renders a ping pong paddle
+            TableTennis = 1,
+            /*ADD MORE AS NEEDED*/
+        }//selection method or the striking object to be drawn. Adds scalability to other sports.
+        public StrikerSelection m_StrikerSelection;
+
+        void Start()
+        {
+            switch (m_StrikerSelection)
+            {
+                case StrikerSelection.TableTennis:
+                    m_currentStriker = Object.Instantiate(m_tableTennisBat, this.transform);
+                    break;
+                case StrikerSelection.Catch:
+                    m_currentStriker = Object.Instantiate(m_catch, this.transform);
+                    break;
+            }
+
+        }
+    }
+}
